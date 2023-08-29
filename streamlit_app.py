bc3c31768f78c532e8a8f98ebd40492ad77abd82
QuantDeveloperUSA <82096803+QuantDeveloperUSA@users.noreply.github.com>
2023-08-28 20:56:15 -0400

    Improving the prompts

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ from langchain.llms import OpenAI
 # for a parrot, we can use this character 🦜
 # for a chain, we can use this character 🔗
 # for a doctor, we can use this character 👨‍⚕️
+# for a PHD Professor, we can use this character 👨‍🏫
+# for an assistant, we can use this character 👨‍💼
 # for an Engineer, we can use this character 👨‍🔧
 # for A.I., we can use this character 🤖
 
@@ -11,16 +13,26 @@ from langchain.llms import OpenAI
 st.title('👨‍⚕️ A.I. Assistant for Doctors')
 
 openai_api_key = st.sidebar.text_input('What is the Magic Word?')
-Context_Prompt = "A Medical doctor is talking to his professional assistant (who happens to be also a PHD in Medicine), about a medical condition. The doctor asks: "
-Contextualize_the_Answer = "The assistant replies: "
+Context_for_assistant_Prompt = "A Medical doctor is talking to his assistant about a medical task. The doctor asks: "
+Contextualize_the_Assistant_Answer = "The assistant replies: "
+
+
+Context_for_PHD_Prompt = "A Medical Doctor asks his PHD Professor: "
+Contextualize_the_PHD_Answer = "The PHD Professor replies: "
+
+
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
   st.info(llm(input_text))
 
-with st.form('my_form'):
-  text = Context_Prompt + st.text_area('Enter text:', 'What is an Aspirin Good for?')  + Contextualize_the_Answer
-  submitted = st.form_submit_button('Submit')
+with st.form('Doctors_form'):
+  assistant_text = Context_for_assistant_Prompt + st.text_area('Enter text:', 'What is an Aspirin Good for?')  + Contextualize_the_Assistant_Answer
+  submitted_to_assistant = st.form_submit_button('Ask the Assistant 👨‍💼')
+
+  PHD_text = Context_for_PHD_Prompt + st.text_area('Enter text:', 'What is an Aspirin Good for?')  + Contextualize_the_PHD_Answer
+  submitted_to_PHD = st.form_submit_button('Ask the PHD Professor 👨‍🏫')
+
   if not openai_api_key.startswith('sk-'):
     st.warning('That is not the right Magic word!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text)
+  if submitted_to_assistant and openai_api_key.startswith('sk-'):
+    generate_response(assistant_text)
