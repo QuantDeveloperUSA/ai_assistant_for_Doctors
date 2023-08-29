f6db1a06259fe4a5cff175200f88cb3df40ca755
QuantDeveloperUSA <82096803+QuantDeveloperUSA@users.noreply.github.com>
2023-08-28 20:36:56 -0400

    Modified to attend doctors

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,25 @@
 import streamlit as st
 from langchain.llms import OpenAI
 
-st.title('🦜🔗 Quickstart App')
+# for a parrot, we can use this character 🦜
+# for a chain, we can use this character 🔗
+# for a doctor, we can use this character 👨‍⚕️
+# for an Engineer, we can use this character 👨‍🔧
+# for A.I., we can use this character 🤖
 
-openai_api_key = st.sidebar.text_input('OpenAI API Key')
+
+st.title('👨‍⚕️ A.I. Assistant for Doctors')
+
+openai_api_key = st.sidebar.text_input('What is the Magic Word?')
 
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
   st.info(llm(input_text))
 
 with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  text = st.text_area('Enter text:', 'please respond in 20 lines: What is an Aspirin Good for?')
   submitted = st.form_submit_button('Submit')
   if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
+    st.warning('That is not the right Magic word!', icon='⚠')
   if submitted and openai_api_key.startswith('sk-'):
     generate_response(text)
